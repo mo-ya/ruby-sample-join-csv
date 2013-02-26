e1ca8ef614d6d9d0a719d10c0fed2dc9a11a2eee
mo-ya <moayao@gmail.com>
2013-02-26 13:29:15 +0900

    Add scripts

---
+++ join_csv_plain.rb
@@ -0,0 +1,49 @@
+#!/usr/bin/ruby
+########################################
+#### Settings
+DELIM=","
+
+########################################
+#### Main Program
+
+if ARGV.size < 3
+  print <<-"EOH"
+
+    USAGE: ruby #{$0} <IN-FILE1> <IN-FILE2> <OUT-FILE>
+
+EOH
+  exit 1
+end
+
+in1 = ARGV[0]
+in2 = ARGV[1]
+out = ARGV[2]
+
+in1lines = File.open(in1).read.split("\n")
+in2lines = File.open(in2).read.split("\n")
+out_io   = File.open(out, "w")
+
+in1size = in1lines.size
+in2size = in2lines.size
+
+if in1size > in2size
+  line_num = in1size
+else
+  line_num = in2size
+end
+
+for n in 1..line_num
+  in1dat = in1lines[n-1]
+  unless in1dat 
+    in1dat = ""
+  end
+
+  in2dat = in2lines[n-1]
+  unless in2dat 
+    in2dat = ""
+  end
+
+  out_io.puts in1dat + DELIM + in2dat
+end
+
+out_io.close
